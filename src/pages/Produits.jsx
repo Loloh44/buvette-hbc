9e7993dc94692b9b2437173e21adfead41b5d720
Loloh44 <pub.lhnh@gmail.com>
2026-04-25 11:15:08 +0200

    Update Produits.jsx

--- src/pages/Produits.jsx
+++ src/pages/Produits.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { supabase } from '../lib/supabase'
 import { fmt } from '../lib/sumup'
-import { useSortable } from '../hooks/useSortable'
+import { useSortable } from '../hooks/useSortable.jsx'
 
 export default function ProduitsPage() {
   const [data, setData] = useState([])
